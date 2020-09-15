90a65fb1985262fe631af8069d27292e96f8f006
MaxenceMillot <maxence.millot@hotmail.fr>
2020-09-15 10:50:16 +0200

    Score isn't updated anymore after game over

--- Assets/Scripts/UI/Score.cs
+++ Assets/Scripts/UI/Score.cs
@@ -16,8 +16,13 @@ public class Score : MonoBehaviour
             distanceRemaining = (end.position.z - player.position.z);
             if (player.position.z > 20 && distanceRemaining > 0)
             {
-                FindObjectOfType<Timer>().timer.text = "";
-                score.text = (distanceRemaining).ToString("0");
+                // If game is not over, write new distance
+                // Otherwise, keep last score
+                if (!FindObjectOfType<GameManager>().isGameOver)
+                {
+                    FindObjectOfType<Timer>().timer.text = "";
+                    score.text = (distanceRemaining).ToString("0");
+                }
             }
             else
             { // prevent score from showing negative number
